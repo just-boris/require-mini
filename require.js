097b24ce99e6cb35a703d9d2fa17483bc300717f
just-boris <just-boris@hotmail.com>
2015-01-14 00:02:33 +0300

    :ski: error handling

--- require.js
+++ require.js
@@ -15,13 +15,16 @@ function loadScript(name, path) {
         el = document.createElement("script");
     deferred.path = path.concat(name);
     pendingModules[name] = deferred;
-    el.onerror = deferred.reject;
+    el.onerror = function() {
+        delete pendingModules[name];
+        deferred.reject(new Error('Error while loading module "'+name+'"'));
+    };
     el.async = true;
     el.src = './' + name + '.js';
     document.getElementsByTagName('body')[0].appendChild(el);
     return deferred.promise;
 }
-function _require(deps, factory, path) {
+function _require(deps, factory, errback, path) {
     return Promise.all(deps.map(function (dependency) {
         if(path.indexOf(dependency) > -1) {
             return Promise.reject(new Error('Circular dependency: '+path.concat(dependency).join(' -> ')));
@@ -32,11 +35,18 @@ function _require(deps, factory, path) {
         return modules[dependency];
     })).then(function (modules) {
         return factory.apply(null, modules);
+    }, function(reason) {
+        if(typeof errback === 'function') {
+            errback(reason);
+        } else {
+            console.error(reason);
+        }
+        return Promise.reject(reason);
     });
 }
 
-function require(deps, factory) {
-    return _require(deps, factory, []);
+function require(deps, factory, errback) {
+    return _require(deps, factory, errback, []);
 }
 
 function define(name, deps, factory) {
@@ -44,12 +54,12 @@ function define(name, deps, factory) {
         factory = deps;
         deps = [];
     }
-    var modulePromise = pendingModules[name],
-        module = _require(deps, factory, modulePromise ? modulePromise.path : []);
-    if(modulePromise) {
-        modulePromise.resolve(module);
+    var deferred = pendingModules[name];
+    if(deferred) {
+        var module = _require(deps, factory, deferred.reject, deferred.path);
+        deferred.resolve(module);
         delete pendingModules[name];
     } else {
-        modules[name] = module;
+        modules[name] = _require(deps, factory, null, []);
     }
 }
--- test/require.spec.js
+++ test/require.spec.js
@@ -1,12 +1,13 @@
+beforeEach(function () {
+    modules = {};
+});
+afterEach(function () {
+    expect(Object.keys(pendingModules).length).toBe(0);
+});
+
 describe("reqiure", function() {
-    beforeEach(function () {
-        modules = {};
-    });
-    afterEach(function () {
-        expect(Object.keys(pendingModules).length).toBe(0);
-    });
-    it("should require modules", function(done) {
-        require(['base/test/fixtures/A', 'base/test/fixtures/B'], function(A, B) {
+    it("should require modules", function (done) {
+        require(['base/test/fixtures/A', 'base/test/fixtures/B'], function (A, B) {
             expect(A).toBe("module A");
             expect(B).toBe("module B");
             done();
@@ -14,25 +15,36 @@ describe("reqiure", function() {
     });
 
     it("should load modules with dependencies", function (done) {
-        require(['base/test/fixtures/C'], function(C) {
+        require(['base/test/fixtures/C'], function (C) {
             expect(C).toBe("module C with module A");
             done();
         });
     });
 
     it("should can predefine modules", function (done) {
-        define('C', function() {
+        define('C', function () {
             return 'module C';
         });
-        require(['C'], function(C) {
+        require(['C'], function (C) {
             expect(C).toBe('module C');
+            done();
+        });
+    });
+});
+
+describe('error handling', function() {
+    it("should report about error in load scripts", function (done) {
+        var onLoad = jasmine.createSpy('onLoad');
+        require(['no-module'], onLoad, function(error) {
+            expect(onLoad).not.toHaveBeenCalled();
+            expect(error.message).toEqual('Error while loading module "no-module"');
             done();
         });
     });
 
     it("should detect circular dependency", function (done) {
         var onLoad = jasmine.createSpy('onLoad');
-        require(['base/test/fixtures/circular A'], onLoad).catch(function(error) {
+        require(['base/test/fixtures/circular A'], onLoad, function(error) {
             expect(onLoad).not.toHaveBeenCalled();
             expect(error.message).toMatch('Circular dependency: ');
             done();
